9a872fd512ad0b4b5a547f470170168f045d679b
nocodenodev <nocodenodev@gmail.com>
2024-09-13 09:49:56 -0300

    Criação do seed do banco de dados

--- ApiCodeSnippets/Data/CodeSnippetsContext.cs
+++ ApiCodeSnippets/Data/CodeSnippetsContext.cs
@@ -15,5 +15,31 @@ namespace ApiCodeSnippets.Data
         }
 
         public DbSet<ApiCodeSnippets.Models.CodeSnippet> CodeSnippet { get; set; } = default!;
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CodeSnippet>().HasData(
+                    new CodeSnippet
+                    {
+                        Id = 1,
+                        Title = "Sum function",
+                        ProgrammingLanguage = "TypeScript",
+                        Tag = "Functions",
+                        Description = "Simple sum function snippet",
+                        Status = "Active",
+                        Snippet = "const sum = (x, y) => {return x + y;}"
+                    },
+                    new CodeSnippet
+                    {
+                        Id = 2,
+                        Title = "Multiply function",
+                        ProgrammingLanguage = "TypeScript",
+                        Tag = "Functions",
+                        Description = "Simple multiply function snippet",
+                        Status = "Active",
+                        Snippet = "const multiply = (x, y) => {return x * y;}"
+                    }
+                );
+        }
     }
 }
---
+++ ApiCodeSnippets/Migrations/20240913123011_seed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCodeSnippets.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CodeSnippets",
+                columns: new[] { "Id", "Description", "ProgrammingLanguage", "Snippet", "Status", "Tag", "Title" },
+                values: new object[] { 1, "Simple sum function snippet", "TypeScript", "const sum = (x, y) => {return x + y;}", "Active", "Functions", "Sum function" });
+
+            migrationBuilder.InsertData(
+                table: "CodeSnippets",
+                columns: new[] { "Id", "Description", "ProgrammingLanguage", "Snippet", "Status", "Tag", "Title" },
+                values: new object[] { 2, "Simple multiply function snippet", "TypeScript", "const multiply = (x, y) => {return x * y;}", "Active", "Functions", "Multiply function" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CodeSnippets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CodeSnippets",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
